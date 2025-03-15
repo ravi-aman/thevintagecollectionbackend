dc5c883f55e896d5b850c6d302befc48629a257e
Ravikant Tiwary <tiwariravikant04@gmail.com>
2025-03-16 02:15:54 +0530

    resolving issue

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-const app = require("../index");  // Import Express app
+const app = require("../index");
 
 module.exports = (req, res) => {
-  return app(req, res);  // Use Express as a serverless function
+  app(req, res);
 };
