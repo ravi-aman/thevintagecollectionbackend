c679082459f021e87d9966a2cb1f28780c01aa23
Ravikant Tiwary <tiwariravikant04@gmail.com>
2025-03-16 01:47:13 +0530

    change API handler to return Express app for serverless function compatibility

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-const app = require("../index");
+const app = require("../index");  // Import Express app
 
 module.exports = (req, res) => {
-    app(req, res);
+  return app(req, res);  // Use Express as a serverless function
 };
